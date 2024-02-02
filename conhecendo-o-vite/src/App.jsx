f04f850984a45b493d9120abd6df66f0b09ecfda
Magno <magno.alefb@gmail.com>
2024-02-02 08:45:29 -0300

    configurando o vite com react

---
+++ conhecendo-o-vite/src/App.jsx
@@ -0,0 +1,14 @@
+export default function App(){
+return(
+  <div>
+    <h1>Hello,Word!!!</h1>
+    <p>Este é meu primeiro projeto usando React e Vite</p>
+    <p>Proximos passos:</p>
+    <ul>
+      <li>Praticar com exercicios</li>
+      <li>Estuda e muito</li>
+    </ul>
+  </div>
+  
+)
+}
