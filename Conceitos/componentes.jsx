368b7115b2b6308a49e1a9a4faef31f9399615c8
Magno <magno.alefb@gmail.com>
2024-02-02 08:44:34 -0300

    conceitos react

---
+++ Conceitos/componentes.jsx
@@ -0,0 +1,13 @@
+//Componentes React funções que retornam jsx
+
+function Button(){
+    return <button>Meu Botão</button>
+}
+
+function Container(){
+    return (
+        <div className="componente">
+        <h1>Hello,Word!!!</h1>
+        </div>
+    )
+}
---
+++ Conceitos/index.jsx
@@ -0,0 +1,13 @@
+const element = <h1>Hello,Word!!!</h1> // Exemplo de codigo jsx
+
+const anotherElement = (
+    <div>
+        <h1>Texto</h1>
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+            <li>4</li>
+        </ul>
+    </div>
+)
