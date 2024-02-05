35b3d756a8816ee59b3bff3e8270a0ca951fa721
Magno <magno.alefb@gmail.com>
2024-02-05 08:38:49 -0300

    estililzação no react

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,11 +1,13 @@
 import { StatusText } from "./componentes/StatusText"
 import { Subtitle } from "./componentes/SubTitle"
 import Title from "./componentes/Title"
+import styles from "./App.module.css"
 
 
 export default function App() {
   return (
-<div>
+<div className={styles.app}
+>
   <Title/>
   <Subtitle/>
   <StatusText/>
--- React/src/componentes/StatusText.jsx
+++ React/src/componentes/StatusText.jsx
@@ -1,6 +1,10 @@
 export const StatusText = () =>  {
     const status = true
     return (
-        <h2>Current status : {status ? 'true' : false} </h2>
+        <h2
+        style={{
+            colors:status ? '#00ff9f' : "#f64348"
+        }}
+        >Current status : {status ? 'true' : false} </h2>
     )
 }
