c2ceee0daf6736703068065b1c29d87923362750
Magno <magno.alefb@gmail.com>
2024-02-13 18:51:56 -0300

    hooks

--- React/hooks/useCounter.js
+++ React/hooks/useCounter.js
@@ -1,11 +1,23 @@
 
 import { useState } from "react";
 
+function getInitialValue(){
+    //Usando essa função no State , toda vez que o state muda 
+    //Vai chama a função o jeito correto e chama a função sendo executada () => 
+    console.log('Obtendo valor inicial')
+    return 1+1
+}
+
 export default function useCounter(){
-    const [count, setCount] = useState(0)
+    //Forma correta de iniciar função () => 
+    const [count, setCount] = useState(() =>getInitialValue())
 
     const increment = () => {
-        setCount(count+1)
+       // setCount(count+1)
+       // setCount(count+1) // Usando outra chama da função modificadora ,  porem o valor ainda não foi modificado
+
+       setCount((currentState) => currentState + 1)
+       setCount((currentState) => currentState + 1) // Usando esse padrão ele leva em consideração os valores atualizados
     }
 
     return { count , increment}
--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,18 +1,63 @@
-import useCounter from "../hooks/useCounter";
+//import useCounter from "../hooks/useCounter";
+
+import { useState } from "react";
 
 //Regra1 : Onde usar os hooks
   //Sempre chama os hooks dentro de funções reacts
 //Rega2 : A ordem dos hooks   
   // Não usar hooks dentro de condicionais
 
+  function getRandom() {
+    return Math.random();
+  }
+
+  
+
+
 export default function App() {
 //Top level da função
-  const {count,increment} = useCounter()
+  //const {count,increment} = useCounter()
+
+  const [copia,setCopia] = useState('Copiar')
+
+  const [values , setValues] = useState()
+
+  //Pegando valor do input pelo State
+
+  const [passwordSize, setPasswordSize] = useState(12)
+  
+  const handleGerarClick = () => {
+    const valueRandom = getRandom();
+    setValues(valueRandom);
+    setCopia('Copiar');
+  };
+
+  const handleCopiarClick = () => {
+    if (values !== undefined) {
+      navigator.clipboard.writeText(values.toString())
+        .then(() => {
+          setCopia('Copiado');
+        })
+        .catch((error) => {
+          console.error('Erro ao copiar para a área de transferência', error);
+        });
+    }
+  };
+
   return (
     <>
    <div>
-    <span>Hello,Word!!!</span>
-    <button onClick={increment}>{count}</button>
+    <span>Gerador de senhas</span>
+    <div>
+      <label htmlFor="passwordSize">Tamanho: </label>
+      <input type="number" id="passwordSize" min={1} value={passwordSize} onChange={(ev) => setPasswordSize(ev.target.value)} /> {/*  modificar valor do input */}
+    </div>
+    <br />
+    <button onClick={handleGerarClick}>Gerar senha de {passwordSize}</button>
+    <button onClick={handleCopiarClick}>{copia}</button>
+    <br />
+    <span>{values}</span>
+    {/* <button onClick={increment}>{count}</button> */}
    </div>
     </>
   );
