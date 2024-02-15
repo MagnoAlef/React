c3db9a624b4d41af4281607a0795fa3844ecc013
Magno <magno.alefb@gmail.com>
2024-02-15 08:23:40 -0300

    useEffect

--- UseEffect/src/App.jsx
+++ UseEffect/src/App.jsx
@@ -1,33 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react"
+
+
+function Counter(){
+  const [counter,setCounter] = useState(0)
+
+  useEffect(() => {
+    console.log("O efeito colateral foi ativado")
+   
+    return () => {
+      console.log('Fazendo a limpeza...')
+      console.log('Componente desmontado')
+    } 
+  
+  },[])
+  
+  return (
+    <>
+      <div>
+      <h1>Hello,Word!!!</h1>
+      <button onClick={() => setCounter(counter + 1)}>Contador: {counter}</button>
+      </div>
+    </>
+  )
+  
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+
+const [show, setShow] = useState(false)
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <div>
+          <label htmlFor="show">Exibir</label>
+          <input type="checkbox" value={show} onChange={() => setShow(!show)} />
+        </div>
+        {
+          show ? (  <Counter/>) : ( null)
+        }
+    
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
